ee2d5e218160a6f9ecd0f90784078cb03aeda763
Jofana21 <jofana.metante@evsu.edu.ph>
2021-03-23 05:33:42 -0700

    Third commit

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ContactServiceService } from '../services/contact-service.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-tab3',
@@ -6,7 +8,56 @@ import { Component } from '@angular/core';
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  contName = "";
+  contNumber = ""
+  contacts: any = [];
 
-  constructor() {}
+
+  constructor(public contactService: ContactServiceService, public alertCtrl: AlertController) { }
+
+  async ngOnInit() {
+    this.contactService.getData().then(data => {
+        this.contacts = data;
+    });
+  }
+
+  saveC() {
+    this.contactService.saveContact({name: this.contName, number: this.contNumber}).then(data => {
+      this.contacts = data;
+      this.clearField()
+    });
+  }
+
+  clearField() {
+    this.contName = "";
+    this.contNumber = "";
+  }
+
+  async showConfirm(cont) {  
+    const confirm = await this.alertCtrl.create({  
+      header: 'Confirm!',  
+      message: 'Confrim to delete?',  
+      buttons: [  
+        {  
+          text: 'Cancel',  
+          role: 'cancel',  
+          handler: () => {  
+            console.log('Confirm Cancel');  
+          }  
+        },  
+        {  
+          text: 'YES',  
+          handler: () => {  
+            console.log('Confirm Okay.'); 
+            let index = this.contacts.indexOf(cont);
+    if(index > -1){
+      this.contacts.splice(index, 1);
+    } 
+          }  
+        }  
+      ]  
+    });  
+    await confirm.present();  
+  }
 
 }
